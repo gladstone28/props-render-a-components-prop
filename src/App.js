447cfc4c0ed192a68c3e5e6177990a5d3b79ee86
Gladstone Rose <gladstonerose@hotmail.com>
2024-07-19 15:41:36 -0500

    modify index.html and add Poruct.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-
-import './App.css';
+import React from "react";
+import Product from "./Product";
 
 function App() {
-  return (
-    <div className="App">
-Check my template
-    </div>
-  );
+  return <Product name="Apple Watch" price={399} rating="4.5/5.0" />;
 }
 
 export default App;
